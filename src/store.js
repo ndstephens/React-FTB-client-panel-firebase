ef50d3a823ed90fe3a02561e4d3bc8d5f9569ecc
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-16 14:35:15 -0500

    refactor store

--- src/store.js
+++ src/store.js
@@ -1,64 +1,54 @@
 import { createStore, combineReducers, compose } from 'redux'
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
-// import firebase from 'firebase'
+import { reduxFirestore, firestoreReducer } from 'redux-firestore'
+
+//* Instead of "import firebase from 'firebase'"...only import what you need
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/firestore'
 // import 'firebase/storage'
-import { reduxFirestore, firestoreReducer } from 'redux-firestore'
-import 'firebase/firestore'
 
-// REDUCERS
+//? import custom REDUCERS
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: 'AIzaSyDv4BBHpGvy4gnc5tYe4TdS-CwKhmL8a6o',
+//* Init Firebase App instance
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'client-panel-redux-nds.firebaseapp.com',
   databaseURL: 'https://client-panel-redux-nds.firebaseio.com',
   projectId: 'client-panel-redux-nds',
-  storageBucket: 'client-panel-redux-nds.appspot.com',
-  messagingSenderId: '737345577599',
-}
+  // storageBucket: 'client-panel-redux-nds.appspot.com',
+  // messagingSenderId: '737345577599',
+})
 
-// react-redux-firebase config
+//* Init Firestore
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+//? react-redux-firebase config
 const rrfConfig = {
   userProfile: 'users',
   useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
 }
 
-// Init firebase instance
-firebase.initializeApp(firebaseConfig)
-
-// Init firestore
-const firestore = firebase.firestore()
-firestore.settings({ timestampsInSnapshots: true })
-// // Old:
-// // const date = snapshot.get('created_at')
-// // New:
-// // const date = snapshot.get('created_at').toDate()
-
-// Add reactReduxFirebase enhancer when making store creator
+//* Add reactReduxFirebase and reduxFirestore enhancers when making store creator
 const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
-  reduxFirestore(firebase), // <- needed if using firestore
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase),
 )(createStore)
 
-// Add firebase to reducers
-const rootReducer = combineReducers({
+//? Combine all reducers
+const reducers = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer, // <- needed if using firestore
+  firestore: firestoreReducer,
 })
-
-// Create initial state
+//? Create initial state
 const initialState = {}
 
-// Create store
+//* Create store
 const store = createStoreWithFirebase(
-  rootReducer,
+  reducers,
   initialState,
   compose(
-    // reactReduxFirebase(firebase),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
