ee8a6b027a88decdb232b7faa8edeb2f8961692d
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-16 21:36:49 -0500

    adds formatPhone helper function

--- src/helpers.js
+++ src/helpers.js
@@ -4,3 +4,21 @@ export const formatMoney = cents => {
     currency: 'USD',
   })
 }
+
+export const formatPhone = phoneNumber => {
+  const removeDashes = phoneNumber.replace(/-/g, '')
+  // limit length to 10 numbers
+  let formattedPhone = removeDashes.slice(0, 10)
+
+  if (formattedPhone.length >= 6) {
+    return `${formattedPhone.slice(0, 3)}-${formattedPhone.slice(
+      3,
+      6,
+    )}-${formattedPhone.slice(6)}`
+  }
+  if (formattedPhone.length >= 3) {
+    return `${formattedPhone.slice(0, 3)}-${formattedPhone.slice(3)}`
+  }
+
+  return formattedPhone
+}
