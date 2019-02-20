42ae94d8adc28c943c6b816f5dc1bc469ad1dce7
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-20 13:08:59 -0500

    add Settings link and add some comments

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -25,11 +25,13 @@ class AppNavbar extends Component {
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
         <div className="container">
+          {/* Client Panel logo/link to home-page */}
           <Link to="/" className="navbar-brand">
             ClientPanel
           </Link>
 
-          {/* 'hamburger' menu button for narrow screens */}
+          {/* 'hamburger' menu button (only visible on small screens) */}
+          {/* displays collapsible nav menue */}
           <button
             className="navbar-toggler"
             type="button"
@@ -38,11 +40,13 @@ class AppNavbar extends Component {
             <span className="navbar-toggler-icon" />
           </button>
 
+          {/* Nav menu that is collapsible on smaller screens */}
           <div
             className={`navbar-collapse ${
               this.state.isMenuClosed ? 'collapse' : ''
             }`}
           >
+            {/* Dashboard button */}
             <ul className="navbar-nav mr-auto">
               {auth.uid ? (
                 <li className="nav-item">
@@ -52,13 +56,23 @@ class AppNavbar extends Component {
                 </li>
               ) : null}
             </ul>
+
+            {/* Links only visible when user is logged in */}
             {auth.uid ? (
               <ul className="navbar-nav ml-auto">
+                {/* display user's email address */}
                 <li className="nav-item">
                   <a href="#!" className="nav-link">
                     {auth.email}
                   </a>
                 </li>
+                {/* Settings button */}
+                <li className="nav-item">
+                  <Link to="/settings" className="nav-link">
+                    Settings
+                  </Link>
+                </li>
+                {/* Logout button */}
                 <li className="nav-item">
                   <a
                     href="#!"
