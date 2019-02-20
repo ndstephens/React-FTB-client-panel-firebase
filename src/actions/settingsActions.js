2e50e4b3bfddff817f312c0215ea7e5f40fbb57c
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-20 14:29:32 -0500

    update setting value in localStoage, use as payload in action

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -4,14 +4,25 @@ import {
   DISABLE_BALANCE_ON_EDIT,
 } from './types'
 
+export const toggleAllowRegistration = () => ({
+  type: ALLOW_REGISTRATION,
+  payload: toggleValueInLocalStorage('allowRegistration'),
+})
+
 export const toggleDisableBalanceOnAdd = () => ({
   type: DISABLE_BALANCE_ON_ADD,
+  payload: toggleValueInLocalStorage('disableBalanceOnAdd'),
 })
 
 export const toggleDisableBalanceOnEdit = () => ({
   type: DISABLE_BALANCE_ON_EDIT,
+  payload: toggleValueInLocalStorage('disableBalanceOnEdit'),
 })
 
-export const toggleAllowRegistration = () => ({
-  type: ALLOW_REGISTRATION,
-})
+//* toggle the current value in Local Storage, return the boolean value to be used as the action payload to update the same property in state
+const toggleValueInLocalStorage = setting => {
+  const settings = JSON.parse(localStorage.getItem('settings'))
+  settings[setting] = !settings[setting]
+  localStorage.setItem('settings', JSON.stringify(settings))
+  return settings[setting]
+}
