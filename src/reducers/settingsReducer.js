bc4f1d05af21570051073b7432a454e9e6dc6807
Nathaniel Stephens <ndstephens@gmail.com>
2019-02-20 14:29:50 -0500

    update setting from action payload, not just toggling

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -4,28 +4,28 @@ import {
   DISABLE_BALANCE_ON_EDIT,
 } from '../actions/types'
 
-const initialState = {
-  allowRegistration: false,
-  disableBalanceOnAdd: true,
-  disableBalanceOnEdit: false,
-}
+// const initialState = {
+//   allowRegistration: false,
+//   disableBalanceOnAdd: true,
+//   disableBalanceOnEdit: false,
+// }
 
-export default (state = initialState, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case ALLOW_REGISTRATION:
       return {
         ...state,
-        allowRegistration: !state.allowRegistration,
+        allowRegistration: action.payload,
       }
     case DISABLE_BALANCE_ON_ADD:
       return {
         ...state,
-        disableBalanceOnAdd: !state.disableBalanceOnAdd,
+        disableBalanceOnAdd: action.payload,
       }
     case DISABLE_BALANCE_ON_EDIT:
       return {
         ...state,
-        disableBalanceOnEdit: !state.disableBalanceOnEdit,
+        disableBalanceOnEdit: action.payload,
       }
     default:
       return state
